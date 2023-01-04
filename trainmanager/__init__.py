afd4c849949b3cdb420e91907f29c879be8ad665
Lavender Shannon <retrodaredevil@gmail.com>
2023-01-04 17:02:54 -0600

    Working code to control green express passenger train

--- trainmanager/__init__.py
+++ trainmanager/__init__.py
@@ -1,27 +1,42 @@
-import asyncio
+from typing import Optional
 
-from bleak import discover, BleakScanner
-from pylgbst import get_connection_bleak
-from pylgbst.hub import MoveHub
-
-
-async def run():
-    # devices = await BleakScanner.discover(timeout=10.0)
-    # print(f"got devices: {devices}")
-    # connection = get_connection_auto()
-    connection = get_connection_bleak(hub_name="Lavender Green")
-    print(f"Got connection: {connection} mac: {connection.hub_mac}")
-    hub = MoveHub(connection)
-    print("Made hub")
-    hub.led.set_color("red")
-    hub.motor_A.timed(1.0, 0.5)
-    print("done")
+from bricknil.const import Color
+from curio import sleep
+from bricknil import attach, start
+from bricknil.hub import PoweredUpHub
+from bricknil.sensor import TrainMotor, LED, Light
+import logging
 
 
-def main():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(run())
+@attach(TrainMotor, name='motor')
+@attach(LED, name='hub_led')
+@attach(Light, name='train_light')
+class GreenTrain(PoweredUpHub):
+
+    def __init__(self, args, **kwargs):
+        super().__init__(args, *kwargs)
+        self.motor: Optional[TrainMotor] = None
+        self.hub_led: Optional[LED] = None
+        self.train_light: Optional[Light] = None
+
+    async def run(self):
+        self.message_info("Running")
+        await self.train_light.set_brightness(100)
+        for i in range(2):
+            self.message_info('Increasing speed')
+            await self.hub_led.set_color(Color.black)
+            await self.motor.ramp_speed(80,5000)
+            await sleep(5)
+            self.message_info('Coming to a stop')
+            await self.hub_led.set_color(Color.red)
+            await self.motor.ramp_speed(0,1000)
+            await sleep(2)
+
+
+async def system():
+    train = GreenTrain('Lavender Green')
 
 
 if __name__ == '__main__':
-    main()
+    logging.basicConfig(level=logging.INFO)
+    start(system)
